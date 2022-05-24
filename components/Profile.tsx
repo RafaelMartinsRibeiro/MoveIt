46eab1407306a8cda5d0a1a68a72a6d092b324d8
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-23 21:04:28 -0300

    Added HomeModule css and Profile component

---
+++ components/Profile.tsx
@@ -0,0 +1,14 @@
+export function Profile() {
+  return (
+    <div>
+      <img
+        src="https://github.com/RafaelMartinsRibeiro.png"
+        alt="Rafael Martins"
+      />
+      <div>
+        <strong>Rafael Martins</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
