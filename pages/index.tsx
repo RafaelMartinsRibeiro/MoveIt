950daf79d3a1e5df7e696878233fb861cce29386
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-23 21:40:55 -0300

    Added CompletedChallenges component and css module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
 
         <div></div>
