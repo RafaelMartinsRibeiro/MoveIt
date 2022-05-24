7c00d649ae8f8feafeb8ef13586e5bcfe64e4ee9
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-24 19:20:00 -0300

    Added button in Countdown component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import Head from "next/head";
+
 import styles from "../styles/pages/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Move It</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
