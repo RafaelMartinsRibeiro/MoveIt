6405987efd4401f132b9df8033fb8a46feae8dcb
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-26 18:49:40 -0300

    Added ChallengesContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "../styles/globals.css";
+
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />;
+    </ChallengesProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
