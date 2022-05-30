edcdd2cf81973c84543db658a8d518fe4a445a06
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-30 11:44:38 -0300

    Update globals.css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&family=Righteous&display=swap"
-            rel="stylesheet"
-          />
         </Head>
 
         <body>
